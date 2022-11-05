47ebde7eac9c6dd486522410a566b80226e69f28
Hardik Naik <hardiknaik7444@gmail.com>
2022-11-05 09:56:19 +0530

    Update TopBar.vue

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -9,7 +9,17 @@
                 </g>
             </svg>
         </div>
-        <div class="searchContainer"><input placeholder="Search"/></div>
+        <div class="searchContainer">
+            <svg viewBox="0 0 24 24" aria-hidden="true"
+                class="logo">
+                <g>
+                    <path
+                        d="M10.25 3.75c-3.59 0-6.5 2.91-6.5 6.5s2.91 6.5 6.5 6.5c1.795 0 3.419-.726 4.596-1.904 1.178-1.177 1.904-2.801 1.904-4.596 0-3.59-2.91-6.5-6.5-6.5zm-8.5 6.5c0-4.694 3.806-8.5 8.5-8.5s8.5 3.806 8.5 8.5c0 1.986-.682 3.815-1.824 5.262l4.781 4.781-1.414 1.414-4.781-4.781c-1.447 1.142-3.276 1.824-5.262 1.824-4.694 0-8.5-3.806-8.5-8.5z">
+                    </path>
+                </g>
+            </svg>
+            <input placeholder="Search" />
+        </div>
     </header>
 </template>
   
@@ -24,6 +34,7 @@ export default {
 header {
     display: flex;
     flex-direction: row;
+    align-items:center;
     width: 100%;
     box-sizing: border-box;
     flex-shrink: 0;
@@ -42,16 +53,25 @@ header {
     background: rgba(255, 255, 255, 0.7);
     color: rgb(45, 56, 67);
 }
-.searchContainer{
-    flex:1;
+
+.searchContainer {
+    flex: 1;
     text-align: center;
 }
-input{
+.logo{
+    height: 24px;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    transform: translate(51%, 63%);
+}
+input {
     width: 50%;
     padding: 13px;
     border-radius: 26px;
     border: 1px solid #cdcdcd;
     background: #f6f6f6;
+    padding-left: 41px;
 
 }
 </style>
